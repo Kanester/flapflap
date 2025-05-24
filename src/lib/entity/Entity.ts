175ea8873c22e825ab536fb5f0d9cbe5b796ea4f
KanesterP <pampagkieven091@gmail.com>
2025-05-24 22:40:37 +0800

    --no-message

---
+++ src/lib/entity/Entity.ts
@@ -0,0 +1,13 @@
+abstract class Entity {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	abstract update(dt: number): void;
+
+	abstract draw(ctx: CanvasRenderingContext2D): void;
+}
