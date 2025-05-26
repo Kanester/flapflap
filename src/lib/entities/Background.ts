03e11e511b69692f046fdf64cb90fc5cb0f3e122
KanesterP <pampagkieven091@gmail.com>
2025-05-26 18:31:14 +0800

    --no-message

--- src/lib/entities/Background.ts
+++ src/lib/entities/Background.ts
@@ -43,7 +43,7 @@ export class Background extends Entity {
 		const imgW = img.width;
 		const imgH = img.height;
 
-		const scale = window.windowSettings.height / imgH;
+		const scale = 368 / imgH;
 		const drawW = imgW * scale;
 		const drawH = window.windowSettings.height;
 
